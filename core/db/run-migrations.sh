4faecf3528fefcfdcef21d6d91c49fdecdae2866
vsevalot <vsevolod.tetervak@playrix.com>
2022-06-18 14:10:45 +0500

    fix: unimportant detail. They used to call it "migrations"

---
+++ core/db/run-migrations.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# run-migrations.sh
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h db -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec alembic upgrade head
